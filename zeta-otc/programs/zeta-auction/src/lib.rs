4d09914388f8eeb0a5c1dbd7a977a5486b33dcb8
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2021-12-23 09:54:34 -0400

    add init underlying

--- zeta-otc/programs/zeta-auction/src/lib.rs
+++ zeta-otc/programs/zeta-auction/src/lib.rs
@@ -4,7 +4,10 @@ use anchor_spl::token::{self, Burn, CloseAccount, Mint, MintTo, Token, TokenAcco
 declare_id!("3ruCKuy5gkAj69A4cvapM6rpeKYbvQvt6esuoC14UZNR");
 
 // seeds
-pub const ESCROW_SEED: &str = "escrow";
+pub const STATE_SEED: &str = "state";
+pub const AUCTION_SEED: &str = "auction";
+pub const MINT_AUTH_SEED: &str = "mint-auth";
+pub const UNDERLYING_SEED: &str = "underlying";
 
 #[program]
 pub mod zeta_auction {
@@ -14,6 +17,19 @@ pub mod zeta_auction {
         ctx: Context<InitializeState>,
         args: InitializeStateArgs,
     ) -> ProgramResult {
+        ctx.accounts.state.state_nonce = args.state_nonce;
+        ctx.accounts.state.mint_auth_nonce = args.mint_auth_nonce;
+        ctx.accounts.state.admin = ctx.accounts.admin.key();
+        Ok(())
+    }
+
+    pub fn initialize_underlying(
+        ctx: Context<InitializeUnderlying>,
+        args: InitializeUnderlyingArgs,
+    ) -> ProgramResult {
+        if ctx.accounts.state.admin != ctx.accounts.admin.key() {
+            return Err(ErrorCode::UnauthorizedAdmin.into())
+        }
         Ok(())
     }
 
@@ -58,7 +74,41 @@ pub mod zeta_auction {
 // accounts
 #[derive(Accounts)]
 #[instruction(args: InitializeStateArgs)]
-pub struct InitializeState {
+pub struct InitializeState<'info> {
+    #[account(
+        init,
+        seeds = [STATE_SEED.as_bytes().as_ref()],
+        bump = args.state_nonce,
+        payer = admin
+    )]
+    pub state: Box<Account<'info, State>>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    #[account(
+        seeds = [MINT_AUTH_SEED.as_bytes().as_ref()],
+        bump = args.mint_auth_nonce,
+    )]
+    pub mint_authority: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(args: InitializeUnderlyingArgs)]
+pub struct InitializeUnderlying<'info> {
+    pub state: Account<'info, State>,
+    #[account(
+        init,
+        seeds = [UNDERLYING_SEED.as_bytes().as_ref(), mint.key().as_ref()],
+        bump = args.underlying_nonce,
+        payer = admin,
+    )]
+    pub underlying: Account<'info, Underlying>,
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[derive(Accounts)]
@@ -95,6 +145,17 @@ pub struct TerminateAuction {
 // args
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct InitializeStateArgs {
+    pub state_nonce: u8,
+    pub mint_auth_nonce: u8,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct InitializeUnderlyingArgs {
+    pub underlying_nonce: u8,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct InitializeAuctionArgs {
     pub amount: u64,
     pub starting_price: u64,
     pub bid_end_time: u64,
@@ -103,8 +164,19 @@ pub struct InitializeStateArgs {
     pub bid_token_nonce: u8,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize)]
-pub struct InitializeAuctionArgs {
+#[account]
+#[derive(Default)]
+pub struct Underlying {
+    pub underlying_nonce: u8,
+    pub mint: Pubkey,
+}
+
+#[account]
+#[derive(Default)]
+pub struct State {
+    pub state_nonce: u8,
+    pub mint_auth_nonce: u8,
+    pub admin: Pubkey,
 }
 
 #[error]
